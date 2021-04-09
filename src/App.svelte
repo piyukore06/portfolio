6e4a7e219fa0ef929cec348c6a26e1a0ff410be7
Priyanka Shete <priyanka.shete@adesso.de>
2021-04-09 13:07:36 +0200

    Added new talks

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,65 @@
 <script>
-	import Heading from './Layout/Heading.svelte' 
-	import Top from './Layout/Top.svelte' 
-	import Bottom from './Layout/Bottom.svelte' 
-	import Social from './Social/Index.svelte' 
-  
-  import Link from './Link.svelte';
-  import Route from './Route.svelte';
+  import Heading from "./Layout/Heading.svelte";
+  import Top from "./Layout/Top.svelte";
+  import Bottom from "./Layout/Bottom.svelte";
+  import Social from "./Social/Index.svelte";
 
+  import Link from "./Link.svelte";
+  import Route from "./Route.svelte";
 </script>
+
 <style>
-.padding {
-  padding-top: 80px;
-}
-nav {
-  margin: 0 auto;
-  position: relative;
-  text-align: center;
-  cursor: pointer;
-}
-.container {
-  position: relative;
-  font-family: Quicksand;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 42rem;
-  padding: 2.625rem 1.3125rem;
-  font-size: 18px;
-  font-weight: 400;
-  margin-bottom: 40px;
-}
-.copyright {
-  font-size: 12px;
-  position: fixed;
-  bottom: calc(var(--frameHeight) - 20px);
-  left: 30px;
-  z-index: 1000;
-}
+  .padding {
+    padding-top: 80px;
+  }
+  nav {
+    margin: 0 auto;
+    position: relative;
+    text-align: center;
+    cursor: pointer;
+  }
+  .container {
+    position: relative;
+    font-family: Quicksand;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 42rem;
+    padding: 2.625rem 1.3125rem;
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 40px;
+  }
+  .copyright {
+    font-size: 12px;
+    position: fixed;
+    bottom: calc(var(--frameHeight) - 20px);
+    left: 30px;
+    z-index: 1000;
+  }
+
+  .love {
+    font-size: 12px;
+    position: fixed;
+    bottom: calc(var(--frameHeight) - 45px);
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1000;
+  }
 </style>
-<Social></Social>
-<Top></Top>
-<Heading></Heading>
-<div class="padding"></div>
+
+<Social />
+<Top />
+<Heading />
+<div class="padding" />
 <nav>
-  <Link page={{path: '/', name: 'About'}} />
-  <Link page={{path: '/work', name: 'Work'}} />
-  <Link page={{path: '/blog', name: 'Blog'}} />
-  <Link page={{path: '/talks', name: 'Talks'}} />
+  <Link page={{ path: '/', name: 'About' }} />
+  <Link page={{ path: '/work', name: 'Work' }} />
+  <Link page={{ path: '/blog', name: 'Blog' }} />
+  <Link page={{ path: '/talks', name: 'Talks' }} />
 </nav>
 <div class="container">
   <Route />
 </div>
-<div class="copyright">© 2020 Priyanka Kore</div>
-<Bottom></Bottom>
+<div class="copyright">© 2021 Priyanka Kore</div>
+<div class="love">Made with ☕️</div>
+<Bottom />
--- src/Data/talks.js
+++ src/Data/talks.js
@@ -2,6 +2,32 @@ import { calendar, location } from './icons'
 export default [
   {
     path: '#',
+    heading: 'Introduction to Cypress',
+    links: [{
+      prefix: calendar,
+      name: 'Apr 09, 2021',
+      path: 'https://cypress-intro.netlify.app/'
+    }, {
+      prefix: location,
+      name: 'Adesso',
+      path: 'https://www.adesso.de/de/index.jsp'
+    }]
+  },
+  {
+    path: '#',
+    heading: 'Typescript: zero to hero',
+    links: [{
+      prefix: calendar,
+      name: 'Feb 21, 2021',
+      path: 'https://typescript-zero-to-hero.netlify.app/'
+    }, {
+      prefix: location,
+      name: 'Adesso',
+      path: 'https://www.adesso.de/de/index.jsp'
+    }]
+  },
+  {
+    path: '#',
     heading: 'Workshop: HTML and CSS for Backend Developers',
     links: [{
       prefix: calendar,
@@ -38,88 +64,88 @@ export default [
     }],
     description: `Website users want a smooth and engaging experience. In this talk we'll learn how browser rendering works and how to navigate DevTools to debug animation performance issues. There are optimal practices to bear in mind that are considerably overlooked. The main reason for this to happen is devs don't get a the picture why they exist and are so vigorously advocated. DevTools are gold mine of information, we are going to explore the features that help us build animations smooth as silk.)`,
   },
-    {
-      path: 'https://www.slideshare.net/piyukore06/debugging-perf-with-devtools-184030862',
-      heading: 'Debugging Web Performance with Chrome dev tools',
-      links: [{
-        prefix: calendar,
-        name: 'Oct 16, 2019',
-        path: `#`
-      }, {
-        prefix: location,
-        name: 'SWK Ruhr',
-        path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/265064533/`
-      }],
-      description: `Performance is about retaining users, improving conversions, user experience. It's about people. DevTools performance tab as well as some hidden gems that you probably had no idea existed.`,
-    },
-    {
-      path: 'https://www.meetup.com/NgNiederrhein/events/263357671/',
-      heading: 'Keep calm and refactor',
-      links: [{
-        prefix: calendar,
-        name: 'Aug 13, 2019',
-        path: `#`
-      }, {
-        prefix: location,
-        name: 'Ng Niederrhein',
-        path: `https://twitter.com/NgNiederrhein`
-      }],
-      description: `JavaScript is just amazing for building complex UIs but it's easy to get lost, feel intimidated and feel out of place. We'll look at various patterns from JS-land. Take a step back, understand the abstraction, and see how we can refactor them in a best possible way.`,
-    },
-    {
-        path: 'https://github.com/piyukore06/graphql',
-        heading: 'Rest to GraphQL',
-        links: [{
-          prefix: calendar,
-          name: 'Mar 20, 2019',
-          path: `#`
-        }, {
-          prefix: location,
-          name: 'SWK Ruhr',
-          path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/258189377/`
-        }],
-        description: 'Frontend App APIs often gets complex as the application grows. Often there are multiple round trips required to fetch data for a view. It is a novel way of structuring the client-server contract.',
-    },
-    {
-        path: 'http://parceljs-new-kid-on-the-block.surge.sh',
-        heading: 'Parcel JS',
-        links: [{
-          prefix: calendar,
-          name: 'Oct 17, 2018',
-          path: `#`
-        }, {
-          prefix: location,
-          name: 'SWK Ruhr',
-          path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/252665300/`
-        }],
-        description: 'Frontend tooling just keeps getting better. Module bundlers can do so much for you. Be it providing you with a wonderful dev experience or optimizing your production build.'
-    },
-    {
-        path: 'http://introduction-cypress-js.surge.sh',
-        heading: 'Cypress JS: E2E done right',
-        links: [{
-          prefix: calendar,
-          name: 'Aug 23, 2018',
-          path: `#`
-        }, {
-          prefix: location,
-          name: 'Mehrkanal GmbH',
-          path: `http://mehrkanal.com/`
-        }],
-        description: 'Looking for a Tool to write E2E, well look no further, the most aweosme features of Cypress make it so easy to write and debug e2e tests as hassel free as you can imagine :)'
-    },
-    {
-      path: 'https://github.com/piyukore06/create-a-webpack-plugin',
-      heading: 'Writing your own webpack plugin',
-      links: [{
-        prefix: calendar,
-        name: 'Feb 19, 2018',
-        path: `#`
-      }, {
-        prefix: location,
-        name: 'Mehrkanal GmbH',
-        path: `http://mehrkanal.com/`
-      }],
-      description: `Webpack is highly configurable tool, you can make it do anything you want. Let's learn together about insides of webpack and how it allows to support the whole ecosystem of plugins and in turn how to bulid one.`
-    }
-  ]
+  {
+    path: 'https://www.slideshare.net/piyukore06/debugging-perf-with-devtools-184030862',
+    heading: 'Debugging Web Performance with Chrome dev tools',
+    links: [{
+      prefix: calendar,
+      name: 'Oct 16, 2019',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'SWK Ruhr',
+      path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/265064533/`
+    }],
+    description: `Performance is about retaining users, improving conversions, user experience. It's about people. DevTools performance tab as well as some hidden gems that you probably had no idea existed.`,
+  },
+  {
+    path: 'https://www.meetup.com/NgNiederrhein/events/263357671/',
+    heading: 'Keep calm and refactor',
+    links: [{
+      prefix: calendar,
+      name: 'Aug 13, 2019',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'Ng Niederrhein',
+      path: `https://twitter.com/NgNiederrhein`
+    }],
+    description: `JavaScript is just amazing for building complex UIs but it's easy to get lost, feel intimidated and feel out of place. We'll look at various patterns from JS-land. Take a step back, understand the abstraction, and see how we can refactor them in a best possible way.`,
+  },
+  {
+    path: 'https://github.com/piyukore06/graphql',
+    heading: 'Rest to GraphQL',
+    links: [{
+      prefix: calendar,
+      name: 'Mar 20, 2019',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'SWK Ruhr',
+      path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/258189377/`
+    }],
+    description: 'Frontend App APIs often gets complex as the application grows. Often there are multiple round trips required to fetch data for a view. It is a novel way of structuring the client-server contract.',
+  },
+  {
+    path: 'http://parceljs-new-kid-on-the-block.surge.sh',
+    heading: 'Parcel JS',
+    links: [{
+      prefix: calendar,
+      name: 'Oct 17, 2018',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'SWK Ruhr',
+      path: `https://www.meetup.com/Softwerkskammer-Ruhrgebiet/events/252665300/`
+    }],
+    description: 'Frontend tooling just keeps getting better. Module bundlers can do so much for you. Be it providing you with a wonderful dev experience or optimizing your production build.'
+  },
+  {
+    path: 'http://introduction-cypress-js.surge.sh',
+    heading: 'Cypress JS: E2E done right',
+    links: [{
+      prefix: calendar,
+      name: 'Aug 23, 2018',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'Mehrkanal GmbH',
+      path: `http://mehrkanal.com/`
+    }],
+    description: 'Looking for a Tool to write E2E, well look no further, the most aweosme features of Cypress make it so easy to write and debug e2e tests as hassel free as you can imagine :)'
+  },
+  {
+    path: 'https://github.com/piyukore06/create-a-webpack-plugin',
+    heading: 'Writing your own webpack plugin',
+    links: [{
+      prefix: calendar,
+      name: 'Feb 19, 2018',
+      path: `#`
+    }, {
+      prefix: location,
+      name: 'Mehrkanal GmbH',
+      path: `http://mehrkanal.com/`
+    }],
+    description: `Webpack is highly configurable tool, you can make it do anything you want. Let's learn together about insides of webpack and how it allows to support the whole ecosystem of plugins and in turn how to bulid one.`
+  }
+]
