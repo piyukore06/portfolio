498741f2e2825ed9ac162e193f8bfe0b7f590c2b
shete <priyanka.shete@adesso.de>
2021-12-28 14:36:52 +0100

    Added curious cases

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,22 +1,27 @@
 export default [
-    {
-      path: 'https://photoeditorsdk.com/html5-sdk',
-      heading: 'PhotoEditor SDK',
-      description: 'A fully customizable photo editor for your app.',
-    },
-    {
-      path: 'https://piyukore06.github.io/2048/',
-      heading: '2048',
-      description: `2048 is a single-player sliding block puzzle game. The game's objective is to slide numbered tiles on a grid to combine them to create a tile with the number 2048.`,
-    },
-    {
-        path: 'https://amateur-generative-art.netlify.com',
-        heading: 'Generative Art',
-        description: `"Generative art" often refers to algorithmic art (algorithmically determined computer generated artwork), but artists can also make it using systems of chemistry, biology, mechanics and robotics, smart materials, manual randomization, mathematics, data mapping, symmetry, tiling, and more.`,
-    },
-    {
-      path: 'https://github.com/piyukore06/awesome-web-perf',
-      heading: 'Awesome web perf collection',
-      description: 'One stop shop for all of your perf problems!!'
-    }
-  ]
+  {
+    path: 'https://photoeditorsdk.com/html5-sdk',
+    heading: 'PhotoEditor SDK',
+    description: 'A fully customizable photo editor for your app.',
+  },
+  {
+    path: 'https://piyukore06.github.io/2048/',
+    heading: '2048',
+    description: `2048 is a single-player sliding block puzzle game. The game's objective is to slide numbered tiles on a grid to combine them to create a tile with the number 2048.`,
+  },
+  {
+    path: 'https://amateur-generative-art.netlify.com',
+    heading: 'Generative Art',
+    description: `"Generative art" often refers to algorithmic art (algorithmically determined computer generated artwork), but artists can also make it using systems of chemistry, biology, mechanics and robotics, smart materials, manual randomization, mathematics, data mapping, symmetry, tiling, and more.`,
+  },
+  {
+    path: 'https://github.com/piyukore06/awesome-web-perf',
+    heading: 'Awesome web perf collection',
+    description: 'One stop shop for all of your perf problems!!'
+  },
+  {
+    path: 'https://curious-cases.netlify.app/',
+    heading: 'Curious Cases',
+    description: 'One stop shop for case conversions in JavaScript',
+  }
+]
